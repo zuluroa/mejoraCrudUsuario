d1419b898f8d9b9211ae7aabdd8d8c986ad6640d
David Zuluaga <zuluroa@gmail.com>
2021-11-20 18:26:16 -0500

    Creacion de endpoint para editar los usuarios en la base de datos

--- src/main/java/com/sofka/demo/controller/UsuarioController.java
+++ src/main/java/com/sofka/demo/controller/UsuarioController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -51,4 +52,10 @@ public class UsuarioController {
 		}
 		return "No se elimino el usuario con el ID \"+id";
 	}
+	
+	@PutMapping("/EditarUsuario")
+	public UsuarioModel editarUsuario(@RequestBody UsuarioModel usuario){
+		return usuarioService.guardarUsuario(usuario);
+	}
+	
 }
--- src/main/java/com/sofka/demo/services/UsuarioService.java
+++ src/main/java/com/sofka/demo/services/UsuarioService.java
@@ -39,4 +39,5 @@ public class UsuarioService {
 			return false;
 		}
 	}
+	
 }
